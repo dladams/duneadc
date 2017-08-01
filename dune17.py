c75ca778368a4840470780bf4cd15660606a3621
David Adams <dladams@bnl.gov>
2017-08-01 16:21:45 -0400

    Add samples to DUNE17-cold.

--- dune17.py
+++ dune17.py
@@ -311,6 +311,7 @@ def dune17cSamples(group=0, isNwf=False, isBad=False, isFail=False, skipSel=Fals
   sams2.append(pre + "D0001_0731")
   sams2.append(pre + "D0002_0731") 
   badsams.append(pre + "D0003_0731")  # Bad waveforms chan 0-7
+  badsams.append(pre + "D0003_0731")  # Bad waveforms chan 0-7
   sams2.append(pre + "D0004_0731")
   sams2.append(pre + "D0005_0731")
   sams2.append(pre + "D0006_0731")
@@ -318,6 +319,8 @@ def dune17cSamples(group=0, isNwf=False, isBad=False, isFail=False, skipSel=Fals
   sams2.append(pre + "D0008_0731")
   sams2.append(pre + "D0009_0731")
   badsams.append(pre + "D0011_0731")  # Bad waveforms
+  sams2.append(pre + "D0014_0801")
+  sams2.append(pre + "D0015_0801")
   if isNwf: outsams = nwfsams
   elif isBad: outsams = badsams + nwfsams + rollsams
   elif isFail: outsams = failsams

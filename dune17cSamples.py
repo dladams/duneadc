aae0f385412005c1b0fa7f8bbb34961a53fb12ee
David Adams <dladams@bnl.gov>
2017-09-26 14:13:02 -0400

    Add std samples.

--- dune17cSamples.py
+++ dune17cSamples.py
@@ -885,27 +885,41 @@ def dune17cSamples(group=0, isNwf=False, isBad=False, isFail=False, skipSel=Fals
   sams2.append(pre + "D0811_0925")
   sams2.append(pre + "D0812_0925")
   sams2.append(pre + "D0813_0925")
-  sams2.append(pre + "D0814_0925")  # 9/26
-  sams2.append(pre + "D0815_0925")  # 9/26
-  sams2.append(pre + "D0816_0925")  # 9/26
-  sams2.append(pre + "D0817_0925")  # 9/26
-  sams2.append(pre + "D0818_0925")  # 9/26
-  sams2.append(pre + "D0819_0925")  # 9/26
-  samsh.append(pre + "D0820_0925")  # 9/26
-  sams2.append(pre + "D0821_0925")  # 9/26
-  sams2.append(pre + "D0823_0925")  # 9/26
-  samsh.append(pre + "D0824_0925")  # 9/26
-  sams2.append(pre + "D0825_0925")  # 9/26
-  sams2.append(pre + "D0826_0925")  # 9/26
-  sams2.append(pre + "D0827_0925")  # 9/26
-  samsh.append(pre + "D0828_0925")  # 9/26
-  sams2.append(pre + "D0829_0925")  # 9/26
-  sams2.append(pre + "D0830_0925")  # 9/26
-  samsh.append(pre + "D0831_0925")  # 9/26
-  samsh.append(pre + "D0833_0925")  # 9/26
-  sams2.append(pre + "D0834_0925")  # 9/26
-  sams2.append(pre + "D0835_0925")  # 9/26
-  sams2.append(pre + "D0838_0926")  # 9/26
+  sams2.append(pre + "D0814_0925")
+  sams2.append(pre + "D0815_0925")
+  sams2.append(pre + "D0816_0925")
+  sams2.append(pre + "D0817_0925")
+  sams2.append(pre + "D0818_0925")
+  sams2.append(pre + "D0819_0925")
+  samsh.append(pre + "D0820_0925")
+  sams2.append(pre + "D0821_0925")
+  sams2.append(pre + "D0823_0925")
+  samsh.append(pre + "D0824_0925")
+  sams2.append(pre + "D0825_0925")
+  sams2.append(pre + "D0826_0925")
+  sams2.append(pre + "D0827_0925")
+  samsh.append(pre + "D0828_0925")
+  sams2.append(pre + "D0829_0925")
+  badsams2.append(pre + "D0830_0925")  # Waveform near zero
+  samsh.append(pre + "D0831_0925")
+  sams2.append(pre + "D0832_0925")  # 9/26
+  samsh.append(pre + "D0833_0925")
+  sams2.append(pre + "D0834_0925")
+  sams2.append(pre + "D0835_0925")
+  sams2.append(pre + "D0837_0926")  # 9/26
+  sams2.append(pre + "D0838_0926")
+  sams2.append(pre + "D0839_0926")  # 9/26
+  samsh.append(pre + "D0840_0926")  # 9/26
+  samsh.append(pre + "D0841_0926")  # 9/26
+  sams2.append(pre + "D0842_0926")  # 9/26
+  samsh.append(pre + "D0843_0926")  # 9/26
+  sams2.append(pre + "D0844_0926")  # 9/26
+  sams2.append(pre + "D0845_0926")  # 9/26
+  samsh.append(pre + "D0846_0926")  # 9/26
+  sams2.append(pre + "D0847_0926")  # 9/26
+  sams2.append(pre + "D0850_0926")  # 9/26
+  sams2.append(pre + "D0853_0926")  # 9/26
+  sams2.append(pre + "D0855_0926")  # 9/26
   if isNwf: outsams = nwfsams
   elif isBad: outsams = badsams + badsams2 + badsamsh + nwfsams + rollsams
   elif isFail: outsams = failsams + failsams2 + failsamsh

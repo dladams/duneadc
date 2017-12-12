9ddd5d83134d9038e9bb46ee8f7b43145f597956
David Adams <dladams@bnl.gov>
2017-12-12 09:02:49 -0500

    Add std samples.

--- dune17cSamples.py
+++ dune17cSamples.py
@@ -1824,6 +1824,39 @@ def dune17cSamples(group=0, isNwf=False, isBad=False, isFail=False, skipSel=Fals
   sams2.append(pre + "D1840_1209")
   sams2.append(pre + "D1841_1209")
   sams2.append(pre + "D1842_1209")
+  samsh.append(pre + "D1843_1209")  # 12/12
+  sams2.append(pre + "D1844_1209")  # 12/12
+  samsh.append(pre + "D1845_1209")  # 12/12
+  sams2.append(pre + "D1846_1209")  # 12/12
+  samsh.append(pre + "D1847_1209")  # 12/12
+  sams2.append(pre + "D1848_1209")  # 12/12
+  samsh.append(pre + "D1849_1209")  # 12/12
+  sams2.append(pre + "D1850_1209")  # 12/12
+  samsh.append(pre + "D1851_1209")  # 12/12
+  sams2.append(pre + "D1852_1209")  # 12/12
+  samsh.append(pre + "D1853_1209")  # 12/12
+  sams2.append(pre + "D1854_1209")  # 12/12
+  sams2.append(pre + "D1855_1211")  # 12/12
+  samsh.append(pre + "D1856_1211")  # 12/12
+  sams2.append(pre + "D1857_1211")  # 12/12
+  samsh.append(pre + "D1858_1211")  # 12/12
+  sams2.append(pre + "D1859_1211")  # 12/12
+  samsh.append(pre + "D1860_1211")  # 12/12
+  sams2.append(pre + "D1861_1211")  # 12/12
+  sams2.append(pre + "D1862_1211")  # 12/12
+  samsh.append(pre + "D1863_1211")  # 12/12
+  sams2.append(pre + "D1864_1211")  # 12/12
+  samsh.append(pre + "D1865_1211")  # 12/12
+  sams2.append(pre + "D1866_1211")  # 12/12
+  sams2.append(pre + "D1868_1211")  # 12/12
+  samsh.append(pre + "D1869_1211")  # 12/12
+  samsh.append(pre + "D1870_1211")  # 12/12
+  sams2.append(pre + "D1871_1211")  # 12/12
+  sams2.append(pre + "D1872_1211")  # 12/12
+  samsh.append(pre + "D1873_1211")  # 12/12
+  samsh.append(pre + "D1875_1211")  # 12/12
+  samsh.append(pre + "D1877_1211")  # 12/12
+  samsh.append(pre + "D1879_1211")  # 12/12
   if isNwf: outsams = nwfsams
   elif isBad: outsams = badsams + badsams2 + badsamsh + nwfsams + rollsams
   elif isFail: outsams = failsams + failsams2 + failsamsh

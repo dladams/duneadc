0e1e492e3d31cedbfd52030e13e20ea6db6ed7c2
David Adams <dladams@bnl.gov>
2018-01-01 09:16:16 -0500

    Add try and std samples.

--- dune17cSamples.py
+++ dune17cSamples.py
@@ -1967,14 +1967,22 @@ def dune17cSamples(group=0, isNwf=False, isBad=False, isFail=False, skipSel=Fals
   sams2.append(pre + "D2006_1228")
   samsh.append(pre + "D2007_1229")
   sams2.append(pre + "D2008_1229")
-  sams2.append(pre + "D2009_1229")  # 12/29
-  samsh.append(pre + "D2010_1229")  # 12/29
-  sams2.append(pre + "D2011_1229")  # 12/29
-  samsh.append(pre + "D2012_1229")  # 12/29
-  sams2.append(pre + "D2013_1229")  # 12/29
-  sams2.append(pre + "D2014_1229")  # 12/29
-  samsh.append(pre + "D2015_1229")  # 12/29
-  samsh.append(pre + "D2016_1229")  # 12/29
+  sams2.append(pre + "D2009_1229")
+  samsh.append(pre + "D2010_1229")
+  sams2.append(pre + "D2011_1229")
+  samsh.append(pre + "D2012_1229")
+  sams2.append(pre + "D2013_1229")
+  sams2.append(pre + "D2014_1229")
+  samsh.append(pre + "D2015_1229")
+  samsh.append(pre + "D2016_1229")
+  samsh.append(pre + "D2017_1229")  # 1/1
+  sams2.append(pre + "D2019_1229")  # 1/1
+  samsh.append(pre + "D2020_1229")  # 1/1
+  sams2.append(pre + "D2021_1229")  # 1/1
+  samsh.append(pre + "D2022_1229")  # 1/1
+  sams2.append(pre + "D2023_1229")  # 1/1
+  samsh.append(pre + "D2024_1229")  # 1/1
+  sams2.append(pre + "D2025_1229")  # 1/1
   if isNwf: outsams = nwfsams
   elif isBad: outsams = badsams + badsams2 + badsamsh + nwfsams + rollsams
   elif isFail: outsams = failsams + failsams2 + failsamsh

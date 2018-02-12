514afafc4b6c763413ebca96781af6a8eece693b
David Adams <dladams@bnl.gov>
2018-02-12 14:20:59 -0500

    Add std samples.

--- dune17cSamples.py
+++ dune17cSamples.py
@@ -2877,6 +2877,13 @@ def dune17cSamples(group=0, isNwf=False, isBad=False, isFail=False, skipSel=Fals
   sams2.append(pre + "D2834_0212")
   samsh.append(pre + "D2835_0212")
   sams2.append(pre + "D2836_0212")
+  samsh.append(pre + "D2837_0212")  # 2/12
+  sams3.append(pre + "D2839_0212")  # 2/12
+  sams3.append(pre + "D2840_0212")  # 2/12
+  samsh.append(pre + "D2841_0212")  # 2/12
+  samsh.append(pre + "D2843_0212")  # 2/12
+  sams3.append(pre + "D2844_0212")  # 2/12
+  sams2.append(pre + "D2845_0212")  # 2/12
   sams2 += pbrsams2
   samsh += pbrsamsh
   sams3 += pbrsams3

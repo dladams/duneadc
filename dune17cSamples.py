6fb4f3d1ffc25ac15e6a1a9fa544d1b533569760
David Adams <dladams@bnl.gov>
2018-02-13 13:47:33 -0500

    Add try and std sample.

--- dune17cSamples.py
+++ dune17cSamples.py
@@ -2917,6 +2917,7 @@ def dune17cSamples(group=0, isNwf=False, isBad=False, isFail=False, skipSel=Fals
   samsh.append(pre + "D2859_0213")  # 2/13
   sams3.append(pre + "D2860_0213")  # 2/13
   sams2.append(pre + "D2861_0213")  # 2/13
+  samsh.append(pre + "D2862_0213")  # 2/13
   sams2 += pbrsams2
   samsh += pbrsamsh
   sams3 += pbrsams3

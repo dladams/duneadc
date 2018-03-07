f7c98031485256d9adf8d55586b611039fc4a584
David Adams <dladams@bnl.gov>
2018-03-07 08:27:34 -0500

    Fix try sample name.

--- dune17dlacSamples.py
+++ dune17dlacSamples.py
@@ -3654,21 +3654,21 @@ def dune17dlacSamples(isBad =False, isFail=False, skipSel=False, skipBad=True, i
   sams.append(pre + "D3227_0305_try2")
   sams.append(pre + "D3228_0305_try1")
   sams.append(pre + "D3229_0305_try1")
-  sams.append(pre + "D3230_0306_try1")  # 3/6
-  sams.append(pre + "D3231_0306_try1")  # 3/6
-  sams.append(pre + "D3232_0306_try1")  # 3/6
-  sams.append(pre + "D3233_0306_try1")  # 3/6
-  sams.append(pre + "D3234_0306_try1")  # 3/6
-  sams.append(pre + "D3235_0306_try1")  # 3/6
-  sams.append(pre + "D3237_0306_try1")  # 3/6
-  sams.append(pre + "D3238_0306_try1")  # 3/6
-  sams.append(pre + "D3238_0306_try2")  # 3/6
+  sams.append(pre + "D3230_0306_try1")
+  sams.append(pre + "D3231_0306_try1")
+  sams.append(pre + "D3232_0306_try1")
+  sams.append(pre + "D3233_0306_try1")
+  sams.append(pre + "D3234_0306_try1")
+  sams.append(pre + "D3235_0306_try1")
+  sams.append(pre + "D3237_0306_try1")
+  badsams.append(pre + "D3238_0306_try1")  # Bad waveforms
+  sams.append(pre + "D3238_0306_try2")
   sams.append(pre + "D329_0306_try1:chip3239")  # 3/6
-  sams.append(pre + "D3239_0306_try1")  # 3/6
-  sams.append(pre + "D3240_0306_try1")  # 3/6
-  sams.append(pre + "D3241_0306_try1")  # 3/6
-  sams.append(pre + "D3242_0306_try1")  # 3/6
-  sams.append(pre + "D3242_0306_try1")  # 3/6
+  sams.append(pre + "D3239_0306_try1")
+  sams.append(pre + "D3240_0306_try1")
+  sams.append(pre + "D3241_0306_try1")
+  sams.append(pre + "D3242_0306_try1")  # Bad waveforms
+  sams.append(pre + "D3242_0306_try2")  # 3/6
   sams.append(pre + "D3243_0306_try1")  # 3/6
   sams.append(pre + "D3244_0306_try1")  # 3/6
   sams.append(pre + "D3245_0306_try1")  # 3/6
